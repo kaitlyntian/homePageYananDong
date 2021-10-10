a18be182f2edf3918588f8f38f6c1d2a223bdab6
NancyDong122463 <54345231+NancyDong122463@users.noreply.github.com>
2021-10-10 15:17:31 -0700

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
   $("img").click(function () {
-    if ($(this).attr("src") === "images/xiaomin1.jpg") {
-      $(this).attr("src", "images/xiaomin2.jpg");
-    } else if ($(this).attr("src") === "images/xiaomin2.jpg") {
-      $(this).attr("src", "images/xiaomin1.jpg");
-    } else if ($(this).attr("src") === "images/drawing1.png") {
-      $(this).attr("src", "images/drawing4.png");
-    } else if ($(this).attr("src") === "images/drawing4.png") {
-      $(this).attr("src", "images/drawing1.png");
-    } else if ($(this).attr("src") === "images/drawing2.png") {
-      $(this).attr("src", "images/drawing5.png");
-    } else if ($(this).attr("src") === "images/drawing5.png") {
-      $(this).attr("src", "images/drawing2.png");
-    } else if ($(this).attr("src") === "images/drawing3.png") {
-      $(this).attr("src", "images/drawing6.png");
+    if ($(this).attr("src") === "images/erhu1.png") {
+      $(this).attr("src", "images/erhu2.png");
+    } else if ($(this).attr("src") === "images/erhu2.png") {
+      $(this).attr("src", "images/erhu1.png");
+    } else if ($(this).attr("src") === "images/movie1.png") {
+      $(this).attr("src", "images/movie2.png");
+    } else if ($(this).attr("src") === "images/movie2.png") {
+      $(this).attr("src", "images/movie1.png");
+    } else if ($(this).attr("src") === "images/travel1.png") {
+      $(this).attr("src", "images/travel2.png");
     } else {
-      $(this).attr("src", "images/drawing3.png");
+      $(this).attr("src", "images/travel1.png");
     }
   });
 });
